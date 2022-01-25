3ec18802d02e0537dc84cb00d9f0520114fbd817
root <root@PANDABURGLAR99.localdomain>
2022-01-25 00:02:01 -0500

    update index stories to test storybook for appointment components

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,14 @@ import Button from "components/Button";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+
 
 
 storiesOf("Button", module)
@@ -134,6 +142,33 @@ storiesOf("InterviewerList", module)
       interviewers={interviewers}
       onChange={action("setInterviewer")}
     />
-  ));
+));
       
-      
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => 
+    <Show 
+    student={"Lydia Miller-Jones"}
+    interviewer={interviewer.name}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    /> )
+  .add("Confirm", () => 
+    <Confirm
+    message={"Delete the appointment?"}
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")} 
+   />)
+   .add("Status", () => 
+   <Status message={"Deleting"}/>)
+   .add("Error", () => 
+   <Error 
+    message={"Could not delete appointment"}
+    onClose={action("onClose")}
+   />);      
