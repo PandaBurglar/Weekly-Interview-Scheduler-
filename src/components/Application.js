a8c8d4123096c7359c111718890fe205b09b6b12
root <root@PANDABURGLAR99.localdomain>
2022-01-27 20:07:05 -0500

    add mock appoinntment data and iterate over it

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,46 @@
 import React, { useState } from "react";
-
 import "components/Application.scss";
+import Appointment from "components/Appointment";
 import DayList from "./DayList";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 const days = [
   {
@@ -24,7 +63,15 @@ const days = [
 export default function Application(props) {
   const [day, setDay] = useState({
     day: "Monday"
-  })
+  });
+  const scheduleList = appointments.map((appointment) => {
+    return (
+      <Appointment
+      key={appointment.id}
+      {...appointment}
+      /> 
+    )
+  });
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +95,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {scheduleList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
