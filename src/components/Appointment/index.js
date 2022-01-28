218ab308eb98eb137afc560266f0777db022a7d9
root <root@PANDABURGLAR99.localdomain>
2022-01-27 19:38:05 -0500

    display appointment using tenary operators

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 
+import "components/Appointment/styles.scss";
 
-function formatAppointment(props){
-  return(
-    (!props.time ? "No Appointments" :`Appointment at ${props.time}`)
-  );
-}
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 export default function Appointment (props) {
   return (
-    <article className="appointment"> {formatAppointment(props)} </article>
+    <article className="appointment"> 
+    <Header 
+      time={props.time}  
+    />
+     {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty/>}
+    </article>
   );
 };
