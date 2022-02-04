75480d64cd31d7500763f8233ee479f53c8d8cc4
root <root@PANDABURGLAR99.localdomain>
2022-02-03 19:32:04 -0500

    create useVisualMode helper hook to go back and transition to next appointment component

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,136 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData(){
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+
+   // update spot function 
+   const spotUpdate = (weekday, day, variable) => {
+    let spot = day.spots;
+    if (weekday === day.name && variable === "REMOVE_SPOT") {
+      return spot - 1;
+    } else if (weekday === day.name && variable === "ADD_SPOT") {
+      return spot + 1;
+    } else {
+      return spot;
+    }
+  };
+  const updateSpots = (weekday, days, variable) => {
+    if (variable === "REMOVE_SPOT") {
+      const updatedStateDayArray = days.map((day) => {
+        return {
+          ...day,
+          spots: spotUpdate(weekday, day, variable),
+        };
+      });
+      return updatedStateDayArray;
+    }
+    if (variable === "ADD_SPOT") {
+      const updatedStateDayArray = days.map((day) => {
+        return {
+          ...day,
+          spots: spotUpdate(weekday, day, variable),
+        };
+      });
+      return updatedStateDayArray;
+    }
+  };
+
+  const setDay = (day) => {
+    setState((prev) => ({
+      ...prev,
+      day,
+    }));
+  };
+
+  useEffect(() => {
+    Promise.all([
+      axios.get(`http://localhost:8001/api/days`),
+      axios.get(`http://localhost:8001/api/appointments`),
+      axios.get(`http://localhost:8001/api/interviewers`),   
+
+    ]).then((all) =>{
+      setState(prev => ({
+        ...prev, 
+        days: all[0].data,
+        appointments: all[1].data, 
+        interviewers: all[2].data
+      })); 
+    })
+  }, []);
+
+   // booking interviews
+   function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+      const spotUpdate = updateSpots(state.day, state.days, "REMOVE_SPOT");
+      setState({
+        ...state,
+        days: spotUpdate,
+        appointments,
+      });
+    });
+   };
+
+  // cancelling appt
+  function cancelInterview(appointment) {
+    console.log("try deleting");
+    const deleteAppointment = {
+      ...state.appointments[appointment],
+      interview: null,
+    };
+    const deleteAppointments = {
+      ...state.appointments,
+      [appointment]: deleteAppointment,
+    };
+    
+    return axios
+      .delete(`/api/appointments/${appointment}`, deleteAppointment)
+      .then(() => {
+        const spotUpdate = updateSpots(state.day, state.days, "ADD_SPOT");
+        setState({
+          ...state,
+          days: spotUpdate,
+          deleteAppointments,
+        });
+      });
+  };
+
+   //function to edit interview
+   function editInterview(appointmentId, interviewObj){
+    const editedAppointment = {
+      ...state.appointments[appointmentId],
+      interview: interviewObj,
+    };
+
+    const editedAppointments = {
+      ...state.appointments,
+      [appointmentId]: editedAppointment,
+    };
+
+    return axios
+    .put(
+      `/api/appointments/${appointmentId}`,
+      editedAppointments[appointmentId]
+    )
+    .then(setState({ ...state, appointments: editedAppointments }));
+  };
+
+  
+  return { state, setDay, bookInterview, cancelInterview, editInterview };
+
+} 
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function useVisualMode (initial) {
+  // const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  const mode = history[history.length -1];
+  
+
+  function transition(mode, replace = false) {
+  
+    setHistory((prev) =>
+      replace ? [...prev.slice(0,-1), mode] : [...prev, mode]
+    );
+  }
+
+  // returns last item
+  function back() {
+    setHistory((prev) => (prev.length > 1 ? prev.slice(0, -1) : prev));
+    // setMode(() => history[history.length -1]);
+  }
+  
+  return {
+    mode, 
+    transition, 
+    back
+  };
+}
+
