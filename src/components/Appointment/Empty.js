1dda92170c2bd16b7deddd7be1a6518bb872a3a2
root <root@PANDABURGLAR99.localdomain>
2022-02-03 20:01:21 -0500

    add indentation

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -5,10 +5,10 @@ export default function Empty (props) {
   return (
     <main className="appointment__add">
     <img
-    className="appointment__add-button"
-    src="images/add.png"
-    alt="Add"
-    onClick={props.onAdd}
+      className="appointment__add-button"
+      src="images/add.png"
+      alt="Add"
+      onClick={props.onAdd}
     />
     </main>
   );
