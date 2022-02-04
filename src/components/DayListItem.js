ed5820942cb324b488d5edd0abf3fd16ffdcd216
root <root@PANDABURGLAR99.localdomain>
2022-02-03 19:34:35 -0500

    no updates

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,15 +2,14 @@ import React from "react";
 import classNames from "classnames";
 import "components/DayListItem.scss";
 
-
-function formatSpots(props){
-  return(
-    (props.spots === 0 ? "no spots remaining" :(props.spots === 1 ? "1 spot remaining" : `${props.spots} spots remaining`))
-  );
-}
-
 export default function DayListItem(props) {
   console.log("dayListItem", props);
+  function formatSpots(props){
+    return(
+      (props.spots === 0 ? "no spots remaining" :(props.spots === 1 ? "1 spot remaining" : `${props.spots} spots remaining`))
+    );
+  }
+  
   const dayClass = classNames("dayClass", {
       'day-list__item': true,
       'day-list__item--selected': props.selected,
