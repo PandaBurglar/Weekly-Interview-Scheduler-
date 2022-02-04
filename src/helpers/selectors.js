d6526b6a92d0cc55a525095749306904941b2969
root <root@PANDABURGLAR99.localdomain>
2022-02-03 19:29:22 -0500

    update getAppointmentsForDay function and add getInterview, getInterviewersForDay helper functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,32 @@
 export function getAppointmentsForDay (state, day) {
-  let dayArr = [];
-  let appForDay = state.days.filter(days => {
-      if(days.name === day) {
-        dayArr = days.appointments.map(apptId => state.appointments[apptId])
-      }
-    })
-    return dayArr;
-}; 
+  const filteredDaysArr = state.days.find(item => day === item.name);
+  if (state.days.length < 1 || filteredDaysArr === undefined) {
+    return [];
+  }
+  const dayArr= filteredDaysArr.appointments.map(id => state.appointments[id]);
+  return dayArr;
+};
+
+export function getInterview(state, interview) {
+  let interviewData = {};
+  const interviewers = state.interviewers;
+  
+  if (!interview) {
+    return null;
+  }
+  interviewData["student"] = interview.student;
+  interviewData["interviewer"] = interviewers[interview.interviewer];
+  
+  return interviewData;
+};
+
+export function getInterviewersForDay(state, day) {
+  const { days, interviewers } = state;
+  const filteredDay = days.find(item => item.name === day);
+  if (days.length < 1 || filteredDay === undefined) {
+    return [];
+  }
+
+  return  filteredDay.interviewers.map( id => interviewers[id] );
+
+};
