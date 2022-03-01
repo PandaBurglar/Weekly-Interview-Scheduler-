05d1b5970775f8ea3a4d0d97e9785ea7f91c8188
root <root@PANDABURGLAR99.localdomain>
2022-03-01 13:31:45 -0500

    fixing editing appointments, adding images to README

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,7 +3,7 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  console.log("dayListItem", props);
+  // console.log("dayListItem", props);
   function formatSpots(props){
     return(
       (props.spots === 0 ? "no spots remaining" :(props.spots === 1 ? "1 spot remaining" : `${props.spots} spots remaining`))
@@ -13,10 +13,11 @@ export default function DayListItem(props) {
   const dayClass = classNames("dayClass", {
       'day-list__item': true,
       'day-list__item--selected': props.selected,
-      "day-list__item--full": props.spots === 0
+      "day-list__item--full": props.spots === 0,
+      "day": data-testid
   });
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} data-testid="day"> 
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}> 
       <h2  className="text--regular"> {props.name}</h2> 
       <h3 className="text--light">{formatSpots(props)} </h3>
     </li>
